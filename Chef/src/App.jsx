d100017add4f9f5cb67936532b887f5254ab52ff
unknown <simmulti385@gmail.com>
2026-08-18 00:08:28 +0500

    interactivity Added

--- Chef/src/App.jsx
+++ Chef/src/App.jsx
@@ -2,12 +2,12 @@
 import Header from "./components/Header"
 import { Fragment } from "react"
 import Main from "./components/Main"
-
+import Item from "./components/item"
 export default function App(){
   return<>
     <Header/>
     <Main/>
-
+  <Item/>
 
   </>
     
--- Chef/src/components/Main.jsx
+++ Chef/src/components/Main.jsx
@@ -1,16 +1,28 @@
 
-export default function Main(props){
+import React from "react"
 
-   const ingrideint=["chicken","Milk","Yougart","Garlic"]
 
-   const IngridientItem=ingrideint.map((ingrideint)=>{
-return <li key={ingrideint}>{ingrideint}</li>
-   })
 
-   function Submmit(){
+export default function Main(){
+
+
+  let [Ingridient,SetIngridient]=React.useState(["Garlic","Chicken","Yougurt","Tomato","Onion"])
+
+  let IngriNew=Ingridient.map((things)=><p key={things}>{things}</p>)
+      
+      
+
+   function Submmit(event){
     event.preventDefault()
+            let formdata= new FormData(event.currentTarget)
+            let value=formdata.get("ingridient")
+        if( !Ingridient.includes(value)){
+      SetIngridient(prev=>[...prev,value])
+        }
     console.log("Submmited!")
    }
+
+
     
    return <main>
         <form onSubmit={Submmit} className="add-ingridient-form">
@@ -20,10 +32,11 @@ return <li key={ingrideint}>{ingrideint}</li>
             aria-label="Add ingrideint"
             name="ingridient"/>
             <button > + Add ingrideint</button>
-        </form>
-        <ul>
-            {IngridientItem}
+            </form>
+             <ul>
+            {IngriNew}
         </ul>
+        
          </main>
       
 
